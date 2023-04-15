a2928f34b2008688af31b54755d8c257f3a0c197
Aadi <79352412+aadix420@users.noreply.github.com>
2023-04-15 17:22:41 +0530

    Update 1.0
    #1 : Rewritten the code.
    #2 : Added a function to download a playlist.

--- downloader.py
+++ downloader.py
@@ -1,36 +1,65 @@
-import pytube
 import os
+import pytube
 import requests
 from tqdm import tqdm
 
-# enter the URL of the YouTube video you want to download
-url = input("Enter the YouTube video URL: ")
+# function to download a single audio file
+def download_audio(url):
+    try:
+        # create a YouTube object and get the audio stream
+        yt = pytube.YouTube(url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
 
-# create a YouTube object and get the audio stream
-yt = pytube.YouTube(url)
-audio_stream = yt.streams.filter(only_audio=True).first()
+        # get the file name
+        file_name = yt.title + '.mp3'
 
-# download the audio stream and show the progress bar
-audio_file = None
-try:
-    response = requests.get(audio_stream.url, stream=True)
-    response.raise_for_status()
-    with open('audio_temp', 'wb') as f:
-        total_size = int(response.headers.get('content-length', 0))
-        progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
-        for data in response.iter_content(chunk_size=1024):
-            progress_bar.update(len(data))
-            f.write(data)
-    audio_file = 'audio_temp'
-except (requests.exceptions.RequestException, pytube.exceptions.PytubeError) as e:
-    print("Error downloading audio:", e)
-    exit()
+        # create the downloads directory if it does not exist
+        if not os.path.exists('downloads'):
+            os.makedirs('downloads')
 
-# convert the audio stream to an MP3 file
-if audio_file is not None:
-    base, ext = os.path.splitext(audio_file)
-    new_file = base + '.mp3'
-    os.rename(audio_file, new_file)
-    print("Audio file saved as:", new_file)
-else:
-    print("Error converting audio: no file was downloaded")
+        # download the audio stream and show the progress bar
+        response = requests.get(audio_stream.url, stream=True)
+        response.raise_for_status()
+        filepath = os.path.join('downloads', file_name)
+        with open(filepath, 'wb') as f:
+            total_size = int(response.headers.get('content-length', 0))
+            progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+            for data in response.iter_content(chunk_size=1024):
+                progress_bar.update(len(data))
+                f.write(data)
+        print("Audio file saved as:", filepath)
+    except (requests.exceptions.RequestException, pytube.exceptions.PytubeError) as e:
+        print("Error downloading audio:", e)
+
+# function to download all audio files from a playlist
+def download_playlist(url):
+    try:
+        # create a YouTube playlist object
+        playlist = pytube.Playlist(url)
+
+        # loop through each video in the playlist and download its audio stream
+        for video in playlist.videos:
+            download_audio(video.watch_url)
+    except pytube.exceptions.PytubeError as e:
+        print("Error downloading playlist:", e)
+
+# main program loop
+while True:
+    print("\n============================")
+    print(" YouTube Audio Downloader")
+    print("============================")
+    print("1. Download a single audio file")
+    print("2. Download all audio files from a playlist")
+    choice = input("Enter your choice (1 or 2): ")
+    if choice == '1':
+        url = input("Enter the YouTube video URL: ")
+        download_audio(url)
+        break
+    elif choice == '2':
+        url = input("Enter the YouTube playlist URL: ")
+        download_playlist(url)
+        break
+    else:
+        print("Invalid choice. Please enter '1' or '2'.")
+
+print("\nThank you for using YouTube Audio Downloader!")
