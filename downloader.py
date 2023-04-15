0f7b47cfa67a454da89758052a32ee0cfb490aba
Aadi <79352412+aadix420@users.noreply.github.com>
2023-04-15 18:55:45 +0530

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -3,6 +3,10 @@ import pytube
 import requests
 from tqdm import tqdm
 from urllib.parse import urlparse
+from colorama import Fore, Back, Style, init
+
+# initialize colorama
+init()
 
 # function to download a single audio file
 def download_audio(url):
@@ -14,7 +18,7 @@ def download_audio(url):
         # get the file name and extension based on user choice
         file_name = yt.title
         while True:
-            audio_format = input("Enter audio format (mp3 or wav): ")
+            audio_format = input(Fore.CYAN + "Enter audio format (mp3 or wav): ")
             if audio_format == 'mp3':
                 file_ext = '.mp3'
                 break
@@ -22,7 +26,7 @@ def download_audio(url):
                 file_ext = '.wav'
                 break
             else:
-                print("Invalid audio format. Please enter mp3 or wav.")
+                print(Fore.RED + "Invalid audio format. Please enter mp3 or wav.")
         file_name += file_ext
 
         # create the downloads directory if it does not exist
@@ -39,9 +43,9 @@ def download_audio(url):
             for data in response.iter_content(chunk_size=1024):
                 progress_bar.update(len(data))
                 f.write(data)
-        print("Audio file saved as:", filepath)
+        print(Fore.GREEN + "Audio file saved as:", filepath)
     except (requests.exceptions.RequestException, pytube.exceptions.PytubeError) as e:
-        print("Error downloading audio:", e)
+        print(Fore.RED + "Error downloading audio:", e)
 
 # function to download all audio files from a playlist
 def download_playlist(url):
@@ -53,41 +57,41 @@ def download_playlist(url):
         for video in playlist.videos:
             download_audio(video.watch_url)
     except pytube.exceptions.PytubeError as e:
-        print("Error downloading playlist:", e)
+        print(Fore.RED + "Error downloading playlist:", e)
 
 # main program loop
 while True:
     try:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("\n============================")
-        print(" YouTube Audio Downloader")
-        print("============================")
-        print("1. Download a single audio file")
-        print("2. Download all audio files from a playlist")
-        choice = input("Enter your choice (1 or 2): ")
+        print(Fore.CYAN + "\n============================")
+        print(Fore.CYAN + " YouTube Audio Downloader")
+        print(Fore.CYAN + "============================")
+        print(Fore.CYAN + "1. Download a single audio file")
+        print(Fore.CYAN + "2. Download all audio files from a playlist")
+        choice = input(Fore.CYAN + "Enter your choice (1 or 2): ")
         if choice == '1':
             while True:
-                url = input("Enter the YouTube video URL: ")
+                url = input(Fore.YELLOW + "Enter the YouTube video URL: ")
                 if "youtube.com" in url.lower() and urlparse(url).scheme in ["http", "https"]:
                     break
                 else:
-                    print("Invalid YouTube URL. Please enter a valid YouTube URL.")
+                    print(Fore.RED + "Invalid YouTube URL. Please enter a valid YouTube URL.")
             download_audio(url)
             input("\nPress Enter to continue...")
         elif choice == '2':
             while True:
-                url = input("Enter the YouTube playlist URL: ")
+                url = input(Fore.YELLOW + "Enter the YouTube playlist URL: ")
                 if "youtube.com" in url.lower() and urlparse(url).scheme in ["http", "https"]:
                     break
                 else:
-                    print("Invalid YouTube URL. Please enter a valid YouTube URL.")
+                    print(Fore.RED + "Invalid YouTube URL. Please enter a valid YouTube URL.")
             download_playlist(url)
             input("\nPress Enter to continue...")
         else:
-            print("Invalid choice. Please enter '1' or '2'.")
+            print(Fore.RED + "Invalid choice. Please enter '1' or '2'.")
             input("\nPress Enter to continue...")
     except KeyboardInterrupt:
-        print("\nProgram canceled by user.")
+        print(Fore.RED + "\nProgram canceled by user.")
         break
 
-print("\nThank you for using YouTube Audio Downloader!")
+print(Fore.GREEN + "\nThank you for using YouTube Audio Downloader! ^_^")
